a33a5664a1a66c1cf61c0814aa441af2658c5626
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2018-02-20 02:13:22 +0300

    Add tables for pipelines

---
+++ KpdApps.Orationi.Messaging.Database/dbo/Tables/PipelineAssemblies.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[PipelineAssemblies] (
+    [Id]       UNIQUEIDENTIFIER NOT NULL,
+    [Assembly] VARBINARY (MAX)  NOT NULL,
+    CONSTRAINT [PK_PipelineAssemblies] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ KpdApps.Orationi.Messaging.Database/dbo/Tables/RegisteredSteps.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[RegisteredSteps] (
+    [Id]             UNIQUEIDENTIFIER CONSTRAINT [DF_RegisteredSteps_Id] DEFAULT (newid()) NOT NULL,
+    [RequestCode]    INT              NOT NULL,
+    [Order]          INT              NOT NULL,
+    [IsAsynchronous] BIT              CONSTRAINT [DF_RegisteredSteps_IsAsynchronous] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_RegisteredSteps] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_RegisteredSteps_RequestCodes] FOREIGN KEY ([RequestCode]) REFERENCES [dbo].[RequestCodes] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
