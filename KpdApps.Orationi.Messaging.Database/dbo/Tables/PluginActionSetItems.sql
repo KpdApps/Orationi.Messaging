c0d81627449557492ba53c415b44c48082c113b6
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2018-03-20 18:37:18 +0300

    #11 Chage db schema

--- KpdApps.Orationi.Messaging.Database/dbo/Tables/PluginActionSetItems.sql
+++ KpdApps.Orationi.Messaging.Database/dbo/Tables/PluginActionSetItems.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[PluginActionSetItems] (
-    [Id]                UNIQUEIDENTIFIER CONSTRAINT [DF_RegisteredSteps_Id] DEFAULT (newid()) NOT NULL,
-    [PluginActionSetId] UNIQUEIDENTIFIER NULL,
-    [RequestCode]       INT              NOT NULL,
-    [PluginTypeId]      UNIQUEIDENTIFIER NOT NULL,
-    [Order]             INT              NOT NULL,
-    [IsAsynchronous]    BIT              CONSTRAINT [DF_RegisteredSteps_IsAsynchronous] DEFAULT ((0)) NOT NULL,
-    [Configuration]     NVARCHAR (MAX)   NULL,
+    [Id]                 UNIQUEIDENTIFIER CONSTRAINT [DF_RegisteredSteps_Id] DEFAULT (newid()) NOT NULL,
+    [PluginActionSetId]  UNIQUEIDENTIFIER NOT NULL,
+    [RegisteredPluginId] UNIQUEIDENTIFIER NOT NULL,
+    [Order]              INT              NOT NULL,
+    [Configuration]      NVARCHAR (MAX)   NULL,
     CONSTRAINT [PK_RegisteredSteps] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_PluginActionSetItems_PluginActionSets] FOREIGN KEY ([PluginActionSetId]) REFERENCES [dbo].[PluginActionSets] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT [FK_PluginRegisteredSteps_PluginTypes] FOREIGN KEY ([PluginTypeId]) REFERENCES [dbo].[RegisteredPlugins] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT [FK_RegisteredSteps_RequestCodes] FOREIGN KEY ([RequestCode]) REFERENCES [dbo].[RequestCodes] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT [FK_PluginActionSetItems_RegisteredPlugins] FOREIGN KEY ([RegisteredPluginId]) REFERENCES [dbo].[RegisteredPlugins] ([Id]),
+    CONSTRAINT [FK_PluginRegisteredSteps_PluginTypes] FOREIGN KEY ([RegisteredPluginId]) REFERENCES [dbo].[RegisteredPlugins] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
 
