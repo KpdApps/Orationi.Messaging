ef058a9f3731cd889cec26a58deb2a3d0062f4ed
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2018-03-21 09:38:06 +0300

    #11 remove async host

--- KpdApps.Orationi.Messaging.ServerConsole/Program.cs
+++ KpdApps.Orationi.Messaging.ServerConsole/Program.cs
@@ -17,12 +17,12 @@ namespace KpdApps.Orationi.Messaging.ServerConsole
 
             ProcessHostManager phm = new ProcessHostManager("localhost", "orationi", "orationi");
             phm.Add(1, true);
-            phm.Add(1, false);
+            //phm.Add(1, false);
 
             Console.WriteLine(" Press [enter] to stop.");
             Console.ReadLine();
             phm.Remove(1, true);
-            phm.Remove(1, false);
+            //phm.Remove(1, false);
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
