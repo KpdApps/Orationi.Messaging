a1bf32a583bdac7510a385693613ac848f612b4c
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2018-03-22 09:04:18 +0300

    #11 update tables schema

--- KpdApps.Orationi.Messaging.Database/dbo/Tables/PluginActionSetItems.sql
+++ KpdApps.Orationi.Messaging.Database/dbo/Tables/PluginActionSetItems.sql
@@ -7,8 +7,10 @@
     CONSTRAINT [PK_RegisteredSteps] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_PluginActionSetItems_PluginActionSets] FOREIGN KEY ([PluginActionSetId]) REFERENCES [dbo].[PluginActionSets] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT [FK_PluginActionSetItems_RegisteredPlugins] FOREIGN KEY ([RegisteredPluginId]) REFERENCES [dbo].[RegisteredPlugins] ([Id]),
-    CONSTRAINT [FK_PluginRegisteredSteps_PluginTypes] FOREIGN KEY ([RegisteredPluginId]) REFERENCES [dbo].[RegisteredPlugins] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT [FK_PluginActionSetItems_RegisteredPlugins1] FOREIGN KEY ([RegisteredPluginId]) REFERENCES [dbo].[RegisteredPlugins] ([Id])
 );
+
+
 
 
 
--- KpdApps.Orationi.Messaging.Database/dbo/Tables/WorkflowActions.sql
+++ KpdApps.Orationi.Messaging.Database/dbo/Tables/WorkflowActions.sql
@@ -5,7 +5,9 @@
     [Description]       NVARCHAR (250)   NOT NULL,
     [Order]             INT              NOT NULL,
     CONSTRAINT [PK_WorkflowActions] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_WorkflowActions_PluginActionSets] FOREIGN KEY ([Id]) REFERENCES [dbo].[PluginActionSets] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT [FK_WorkflowActions_PluginActionSets] FOREIGN KEY ([PluginActionSetId]) REFERENCES [dbo].[PluginActionSets] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT [FK_WorkflowActions_Workflows] FOREIGN KEY ([WorkflowId]) REFERENCES [dbo].[Workflows] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
 
