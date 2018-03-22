885ed44252add3fa5dce01c010eb43450360bef1
Denis Vostrikov <eagl@bmail.ru>
2018-03-22 17:10:32 +0300

    #12 - Use dafult port win NT, 5000

--- KpdApps.Orationi.WinNTHostService/Program.cs
+++ KpdApps.Orationi.WinNTHostService/Program.cs
@@ -20,12 +20,13 @@ namespace KpdApps.Orationi.WinNTHostService
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
+            // По умолчанию служба слушает 5000 порт
             WebHost.CreateDefaultBuilder(args)
-                .UseHttpSys(options =>
-                {
-                    options.Authentication.AllowAnonymous = true;
-                    options.UrlPrefixes.Add("http://localhost:7000");
-                })
+                //.UseHttpSys(options =>
+                //{
+                //    options.Authentication.AllowAnonymous = true;
+                //    options.UrlPrefixes.Add("http://localhost:7000");
+                //})
                 .UseStartup<Startup>()
                 .Build();
     }
