33458d258ca2d93b3a6b4afab873be71397b44dd
Denis Vostrikov <eagl@bmail.ru>
2018-03-31 00:52:36 +0300

    #17 - Completed rest service, and change target framework on messaging.common

--- KpdApps.Orationi.Messaging.Core/AuthorizeHelpers.cs
+++ KpdApps.Orationi.Messaging.Core/AuthorizeHelpers.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using KpdApps.Orationi.Messaging.Common.Models;
 using KpdApps.Orationi.Messaging.DataAccess;
 using KpdApps.Orationi.Messaging.DataAccess.Models;
@@ -10,15 +9,6 @@ namespace KpdApps.Orationi.Messaging.Core
 {
     public static class AuthorizeHelpers
     {
-        /// <summary>
-        /// Проверика авторизации пользователя или системы при обращении к шине
-        /// </summary>
-        /// <param name="context">HttpContext <see cref="HttpContext"/></param>
-        /// <param name="requestCode">Код запроса</param>
-        /// <param name="token">Секретный ключ, по которому производится авторизация</param>
-        /// <param name="response">Ответ</param>
-        /// <param name="externalSystem">Внешняя система, которая выполнила запрос</param>
-        /// <returns></returns>
         public static bool IsAuthorized<T>(OrationiDatabaseContext dbContext,
             string token,
             int requestCode,
--- KpdApps.Orationi.Messaging.Rest/Controllers/MessagingController.cs
+++ KpdApps.Orationi.Messaging.Rest/Controllers/MessagingController.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using KpdApps.Orationi.Messaging.Core;
+using KpdApps.Orationi.Messaging.DataAccess;
 
 namespace KpdApps.Orationi.Messaging.Rest.Controllers
 {
     [RoutePrefix("api/rest/messaging")]
-    public class MessagingController : ApiController
+    public class MessagingController : ApiController, IMessagingService
     {
+        private OrationiDatabaseContext _dbContext;
+
+        public MessagingController()
+        {
+            _dbContext = new OrationiDatabaseContext();
+        }
+
         [HttpGet]
         [Route("version")]
         public string GetVersion()
@@ -17,8 +26,88 @@ namespace KpdApps.Orationi.Messaging.Rest.Controllers
             return "v.1.0";
         }
 
+        [HttpGet]
+        [Route("{requestId}")]
+        public Common.Models.Response GetResponse(Guid requestId)
+        {
+            if (!AuthorizeHelpers.IsAuthorized(_dbContext,
+                GetTokenValue(),
+                requestId,
+                out Common.Models.Response response,
+                out var externalSystem))
+            {
+                ActionContext.Response.StatusCode = HttpStatusCode.Forbidden;
+                return response;
+            }
 
+            IncomingMessageProcessor imp = new IncomingMessageProcessor(_dbContext, externalSystem);
+            response = imp.GetResponse(requestId);
+            return response;
+            
+        }
 
+        [HttpGet]
+        [Route("status/{requestId}")]
+        public Common.Models.Response GetStatus(Guid requestId)
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPost]
+        [Route("sync")]
+        public Common.Models.Response ExecuteRequest([FromBody]Common.Models.Request request)
+        {
+            if (!AuthorizeHelpers.IsAuthorized(
+                _dbContext,
+                GetTokenValue(),
+                request.Code,
+                out Common.Models.Response response,
+                out var externalSystem))
+            {
+                ActionContext.Response.StatusCode = HttpStatusCode.Forbidden;
+                return response;
+            }
+
+            IncomingMessageProcessor imp = new IncomingMessageProcessor(_dbContext, externalSystem);
+            response = imp.Execute(request);
+            return response;
+
+        }
+
+        [HttpPost]
+        [Route("async")]
+        public Common.Models.ResponseId ExecuteRequestAsync([FromBody]Common.Models.Request request)
+        {
+            if (!AuthorizeHelpers.IsAuthorized(_dbContext,
+                GetTokenValue(),
+                request.Code,
+                out Common.Models.ResponseId response,
+                out var externalSystem))
+            {
+                ActionContext.Response.StatusCode = HttpStatusCode.Forbidden;
+                return response;
+            }
+
+            IncomingMessageProcessor imp = new IncomingMessageProcessor(_dbContext, externalSystem);
+            response = imp.ExecuteAsync(request);
+            return response;
+        }
+
+        [HttpPost]
+        [Route("request")]
+        public Common.Models.ResponseId SendRequest([FromBody]Common.Models.Request request)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("xsd/{requestCode}")]
+        public Common.Models.Response GetXsd(int requestCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        [NonAction]
         private string GetTokenValue()
         {
             return Request.Headers.GetValues("Token").FirstOrDefault();
