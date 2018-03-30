28835a25e56e40105525e6a581b717e3de1e0c9d
Denis Vostrikov <eagl@bmail.ru>
2018-03-30 23:35:58 +0300

    #17 - Rest

--- KpdApps.Orationi.Messaging.Rest/Controllers/MessagingController.cs
+++ KpdApps.Orationi.Messaging.Rest/Controllers/MessagingController.cs
@@ -1,74 +1,27 @@
 ﻿using System;
-using KpdApps.Orationi.Messaging.Common.Models;
-using KpdApps.Orationi.Messaging.Core;
-using KpdApps.Orationi.Messaging.DataAccess;
-using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace KpdApps.Orationi.Messaging.Rest.Controllers
 {
-    [Route("api/rest/[controller]")]
-    public class MessagingController : Controller, IMessagingService
+    [RoutePrefix("api/rest/messaging")]
+    public class MessagingController : ApiController
     {
-        OrationiMessagingContext _dbContext;
-
-        public MessagingController(OrationiMessagingContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        [HttpGet("version")]
+        [HttpGet]
+        [Route("version")]
         public string GetVersion()
         {
             return "v.1.0";
         }
 
-        [HttpGet("{requestId}")]
-        public Response GetResponse(Guid requestId)
+
+
+        private string GetTokenValue()
         {
-            IncomingMessageProcessor imp = new IncomingMessageProcessor(_dbContext, HttpContext);
-            Response response = imp.GetResponse(requestId);
-            return response;
-        }
-
-        [HttpGet("status/{requestId}")]
-        public Response GetStatus(Guid requestId)
-        {
-            throw new NotImplementedException();
-        }
-
-        [HttpPost("sync")]
-        public Response ExecuteRequest([FromBody]Request request)
-        {
-            // Отдаем запрос в процессор, дальше он сам
-            IncomingMessageProcessor imp = new IncomingMessageProcessor(_dbContext, HttpContext);
-            Response response = imp.Execute(request);
-            return response;
-        }
-
-        [HttpPost("async")]
-        public ResponseId ExecuteRequestAsync([FromBody]Request request)
-        {
-            IncomingMessageProcessor imp = new IncomingMessageProcessor(_dbContext, HttpContext);
-            ResponseId response = imp.ExecuteAsync(request);
-            return response;
-        }
-
-        [HttpPost("request")]
-        public ResponseId SendRequest([FromBody]Request request)
-        {
-            throw new NotImplementedException();
-        }
-
-        [HttpGet("xsd/{requestCode}")]
-        public Response GetXsd(int requestCode)
-        {
-            var response = new Response();
-            if (!HttpContext.IsAuthorized(_dbContext, requestCode, response, out var externalSystem))
-                return response;
-
-            response.Body = "test";
-
-            return response;
+            return Request.Headers.GetValues("Token").FirstOrDefault();
         }
     }
 }
