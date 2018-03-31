f1443dfe3fc62233038c61b31f8aba71f132c1a0
Denis Vostrikov <eagl@bmail.ru>
2018-03-31 21:38:10 +0300

    #17 - Add logger into service

--- KpdApps.Orationi.Service.PluginHost/PluginHost.cs
+++ KpdApps.Orationi.Service.PluginHost/PluginHost.cs
@@ -7,22 +7,29 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
 
 namespace KpdApps.Orationi.Service.PluginHost
 {
     public partial class PluginHost : ServiceBase
     {
+        public static readonly ILog log = LogManager.GetLogger(typeof(PluginHost));
+
         public PluginHost()
         {
             InitializeComponent();
+            XmlConfigurator.Configure();
         }
 
         protected override void OnStart(string[] args)
         {
+            log.Info("Service started");
         }
 
         protected override void OnStop()
         {
+            log.Info("Service stopped");
         }
     }
 }
