dc527239d078428c171b234088c8550a22cf2b1a
Denis Vostrikov <eagl@bmail.ru>
2018-04-04 13:38:11 +0300

    #17 - Add logic in WorkflowInit

--- WorkflowsInit/Infrastructure/WorkflowAction.cs
+++ WorkflowsInit/Infrastructure/WorkflowAction.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace WorkflowsInit.Infrastructure
 {
     public class WorkflowAction
     {
+        [JsonProperty("name")]
+        public string Name { get; set; }
         [JsonProperty("description")]
         public string Description { get; set; }
         [JsonProperty("order")]
--- WorkflowsInit/Infrastructure/WorkflowInfo.cs
+++ WorkflowsInit/Infrastructure/WorkflowInfo.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace WorkflowsInit.Infrastructure
@@ -12,24 +8,8 @@ namespace WorkflowsInit.Infrastructure
         [JsonProperty("name")]
         public string Name { get; set; }
         [JsonProperty("requestCode")]
-        public int RequestCode { get; set; }
+        public RequestCode RequestCode { get; set; }
         [JsonProperty("workflowActions")]
         public List<WorkflowAction> WorkflowActions { get; set; }
-
-        public void Add()
-        {
-            /*
-            var workflow = Workflows.FirstOrDefault(wf => wf.Name == this.Name);
-            if (workflow is null)
-            {
-                Workflows.InsertOnSubmit(new Workflows
-                {
-                    Name = this.Name,
-                    RequestCode = this.RequestCode
-                });
-                SubmitChanges();
-            }
-            */
-        }
     }
 }
