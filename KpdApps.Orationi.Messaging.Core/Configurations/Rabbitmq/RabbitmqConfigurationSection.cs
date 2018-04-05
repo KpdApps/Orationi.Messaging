5bb232cbfc59d0ce1c599af380dd0696c43959b9
Denis Vostrikov <eagl@bmail.ru>
2018-04-05 16:49:09 +0300

    #22 - Add rabbitmq configuration

---
+++ KpdApps.Orationi.Messaging.Core/Configurations/Rabbitmq/RabbitmqConfigurationSection.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+
+namespace KpdApps.Orationi.Messaging.Core.Configurations.Rabbitmq
+{
+    public class RabbitmqConfigurationSection : ConfigurationSection
+    {
+        private const string SectionName = "rabbitmq";
+        private const string HostNameKey = "hostName";
+        private const string UserNameKey = "userName";
+        private const string PasswordKey = "password";
+
+        public static RabbitmqConfigurationSection GetConfiguration()
+        {
+            return (RabbitmqConfigurationSection)ConfigurationManager.GetSection(SectionName);
+        }
+
+        [ConfigurationProperty(HostNameKey)]
+        public string HostName
+        {
+            get => (string)this[HostNameKey];
+            set => this[HostNameKey] = (object)value;
+        }
+
+        [ConfigurationProperty(UserNameKey)]
+        public string UserName
+        {
+            get => (string)this[UserNameKey];
+            set => this[UserNameKey] = (object)value;
+        }
+
+        [ConfigurationProperty(PasswordKey)]
+        public string Password
+        {
+            get => (string)this[PasswordKey];
+            set => this[PasswordKey] = (object)value;
+        }
+    }
+}
