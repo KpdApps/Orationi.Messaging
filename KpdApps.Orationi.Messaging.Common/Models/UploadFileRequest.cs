1febf0883763bdfddf50fc0e886ef740039316b1
Ivan Konstantinov <konstantinov@exportcenter.ru>
2018-05-04 18:28:29 +0300

    #1060 перенес контракт загрузки файла в Commons, добавил package

--- KpdApps.Orationi.Messaging.Common/Models/UploadFileRequest.cs
+++ KpdApps.Orationi.Messaging.Common/Models/UploadFileRequest.cs
@@ -3,13 +3,19 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web.Http;
+using System.Xml.Linq;
 
-namespace KpdApps.Orationi.Messaging.Rest.Models
+namespace KpdApps.Orationi.Messaging.Common.Models
 {
-	public class UploadFileInfo
+	public class UploadFileRequest
 	{
-		public Guid MessageId { get; set; }
+		public Guid ObjectId { get; set; }
+
+		public int ObjectCode { get; set; }
+
 		public string FileType { get; set; }
+
+		public int RequsetCode { get; set; }
 
 		private const int MaxSharePointFileNameLegth = 250;
 
@@ -27,6 +33,18 @@ namespace KpdApps.Orationi.Messaging.Rest.Models
 
 			if (forbidenExtensions.Contains(fileExtension.ToLower()))
 				throw new HttpResponseException(HttpStatusCode.BadRequest);
+		}
+
+		public string ToXmlString(Guid messageId)
+		{
+			var xmlBody = new XElement("UploadFileRequest",
+				new XElement("MessageId", messageId),
+				new XElement("ObjectId", ObjectId),
+				new XElement("ObjectCode", ObjectCode),
+				new XElement("FileType", FileType)
+			);
+
+			return @"<?xml version=""1.0"" encoding=""utf-8""?>" + xmlBody.ToString(SaveOptions.DisableFormatting);
 		}
 	}
 }
