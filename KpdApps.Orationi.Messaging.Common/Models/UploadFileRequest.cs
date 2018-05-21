ef16a1f6563884780303aacbd8cab9fff6d0f804
Ivan Konstantinov <konstantinov@exportcenter.ru>
2018-05-21 11:41:24 +0300

    #27 exception throws message and code instead of only code

--- KpdApps.Orationi.Messaging.Common/Models/UploadFileRequest.cs
+++ KpdApps.Orationi.Messaging.Common/Models/UploadFileRequest.cs
@@ -3,52 +3,62 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using System.Xml.Linq;
 
 namespace KpdApps.Orationi.Messaging.Common.Models
 {
-	public class UploadFileRequest
-	{
-		[JsonProperty("ObjectId")]
-		public Guid ObjectId { get; set; }
+    public class UploadFileRequest
+    {
+        [JsonProperty("ObjectId")]
+        public Guid ObjectId { get; set; }
 
-		[JsonProperty("ObjectCode")]
-		public int ObjectCode { get; set; }
+        [JsonProperty("ObjectCode")]
+        public int ObjectCode { get; set; }
 
-		[JsonProperty("FileType")]
-		public string FileType { get; set; }
+        [JsonProperty("FileType")]
+        public string FileType { get; set; }
 
-		[JsonProperty("RequsetCode")]
-		public int RequsetCode { get; set; }
+        [JsonProperty("RequsetCode")]
+        public int RequsetCode { get; set; }
 
-		private const int MaxSharePointFileNameLegth = 250;
+        private const int MaxSharePointFileNameLegth = 250;
 
-		public static void ValidateFileName(string fileName)
-		{
-			if (string.IsNullOrEmpty(fileName))
-				throw new HttpResponseException(HttpStatusCode.BadRequest);
+        public static void ValidateFileName(string fileName, HttpRequestMessage request)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                throw new HttpResponseException(request.CreateResponse(
+                    HttpStatusCode.BadRequest,
+                    new Response { IsError = true, Error = "Передайте не пустое имя файла" })
+                );
 
-			if (fileName.Length > MaxSharePointFileNameLegth)
-				throw new HttpResponseException(HttpStatusCode.BadRequest);
+            if (fileName.Length > MaxSharePointFileNameLegth)
+                throw new HttpResponseException(request.CreateResponse(
+                    HttpStatusCode.BadRequest,
+                    new Response { IsError = true, Error = $"Имя файла слишком длинное, максимум {MaxSharePointFileNameLegth} символов" })
+                );
 
-			string[] forbidenExtensions = { ".exe", ".dll" };
+            string[] forbidenExtensions = { ".exe", ".dll" };
 
-			var fileExtension = Path.GetExtension(fileName);
+            var fileExtension = Path.GetExtension(fileName);
 
-			if (forbidenExtensions.Contains(fileExtension.ToLower()))
-				throw new HttpResponseException(HttpStatusCode.BadRequest);
-		}
+            if (forbidenExtensions.Contains(fileExtension.ToLower()))
+                throw new HttpResponseException(request.CreateResponse(
+                    HttpStatusCode.BadRequest,
+                    new Response { IsError = true, Error = $"Нельзя загружать файлы с расширением {fileExtension}" })
+                );
+        }
 
-		public string ToXmlString()
-		{
-			var xmlBody = new XElement("UploadFileRequest",
-				new XElement("ObjectId", ObjectId),
-				new XElement("ObjectCode", ObjectCode),
-				new XElement("FileType", FileType)
-			);
+        public string ToXmlString()
+        {
+            var xmlBody = new XElement("UploadFileRequest",
+                new XElement("ObjectId", ObjectId),
+                new XElement("ObjectCode", ObjectCode),
+                new XElement("FileType", FileType)
+            );
 
-			return @"<?xml version=""1.0"" encoding=""utf-8""?>" + xmlBody.ToString(SaveOptions.DisableFormatting);
-		}
-	}
+            return @"<?xml version=""1.0"" encoding=""utf-8""?>" + xmlBody.ToString(SaveOptions.DisableFormatting);
+        }
+    }
 }
