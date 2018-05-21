8ad19ae22fb8e262258a02ec0d9d14ae2c5b0abc
Ivan Konstantinov <konstantinov@exportcenter.ru>
2018-05-21 11:43:37 +0300

    #27 server console update to exclude code commenting during debugging

--- KpdApps.Orationi.Messaging.ServerConsole/Program.cs
+++ KpdApps.Orationi.Messaging.ServerConsole/Program.cs
@@ -1,37 +1,71 @@
-﻿using KpdApps.Orationi.Messaging.DataAccess;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using KpdApps.Orationi.Messaging.DataAccess;
 using KpdApps.Orationi.Messaging.ServerCore.ProcessHosts;
 
 namespace KpdApps.Orationi.Messaging.ServerConsole
 {
     class Program
     {
+        private static readonly bool ShouldProcessSingleRequestCode = true;
+        private static readonly int RequestCode = 60109;
+
         static void Main(string[] args)
         {
             ProcessHostManager processHostManager = new ProcessHostManager("localhost", "orationi", "orationi");
-			//List<(int RequestCode, bool IsSync)> plugins = plugins = new List<(int RequestCode, bool IsSync)>();
 
-			//using (var dbContext = new OrationiDatabaseContext())
-			//{
-			//    foreach (var requestCode in dbContext.RequestCodes)
-			//    {
-			//        plugins.AddRange(new[] { (requestCode.Id, true), (requestCode.Id, false) });
-			//    }
-			//}
+            if (ShouldProcessSingleRequestCode)
+            {
+                SingleRequestCodeProcessing(processHostManager, RequestCode);
+            }
+            else
+            {
+                MultipleRequestCodesProcessing(processHostManager);
+            }
 
-			//plugins.ForEach(p => processHostManager.Add(p.RequestCode, p.IsSync));
-			processHostManager.Add(60109, false);
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
 
-			Console.WriteLine(" Press [enter] to stop.");
+        /// <summary>
+        /// Process single plugin. Method for debugging mostly.
+        /// </summary>
+        /// <param name="processHostManager">RabbitMQ Instance</param>
+        /// <param name="requestCode">Reqeust code number to be processed</param>
+        private static void SingleRequestCodeProcessing(ProcessHostManager processHostManager, int requestCode)
+        {
+            processHostManager.Add(requestCode, false);
+
+            Console.WriteLine($"Processing RequestCode {requestCode}");
+            Console.WriteLine("Press [enter] to stop.");
             Console.ReadLine();
 
-			//TODO: разобраться почему после нажатия Enter не происходит завершения работы хост менеджера
-			//plugins.ForEach(p => processHostManager.Remove(p.RequestCode, p.IsSync));
-			processHostManager.Remove(60109, false);
+            processHostManager.Remove(requestCode, false);
+        }
 
-			Console.WriteLine(" Press [enter] to exit.");
+        /// <summary>
+        /// Process all plugins in DB.
+        /// </summary>
+        /// <param name="processHostManager">RabbitMQ Instance</param>
+        private static void MultipleRequestCodesProcessing(ProcessHostManager processHostManager)
+        {
+            List<(int RequestCode, bool IsSync)> plugins = plugins = new List<(int RequestCode, bool IsSync)>();
+
+            using (var dbContext = new OrationiDatabaseContext())
+            {
+                foreach (var requestCode in dbContext.RequestCodes)
+                {
+                    plugins.AddRange(new[] { (requestCode.Id, true), (requestCode.Id, false) });
+                }
+            }
+
+            plugins.ForEach(p => processHostManager.Add(p.RequestCode, p.IsSync));
+
+            Console.WriteLine(" Press [enter] to stop.");
             Console.ReadLine();
+
+            //TODO: разобраться почему после нажатия Enter не происходит завершения работы хост менеджера
+            plugins.ForEach(p => processHostManager.Remove(p.RequestCode, p.IsSync));
         }
     }
 }
