aa8d3f70b5c52ac4abc599cb1b9cf28de7e7539f
Ivan Konstantinov <konstantinov@exportcenter.ru>
2018-05-21 13:02:21 +0300

    #27 Spaces instead Tabs AND If (error) → return only processingError.Error

--- KpdApps.Orationi.Messaging.Core/ResponseGenerator.cs
+++ KpdApps.Orationi.Messaging.Core/ResponseGenerator.cs
@@ -7,130 +7,130 @@ using KpdApps.Orationi.Messaging.ServerCore.Workflow;
 
 namespace KpdApps.Orationi.Messaging.Core
 {
-	public class ResponseGenerator
-	{
-		private readonly OrationiDatabaseContext _dbContext;
-		private readonly Guid _requestId;
+    public class ResponseGenerator
+    {
+        private readonly OrationiDatabaseContext _dbContext;
+        private readonly Guid _requestId;
 
-		public ResponseGenerator(OrationiDatabaseContext context, Guid requestId)
-		{
-			if (context == null)
-				throw new ArgumentNullException(nameof(context));
+        public ResponseGenerator(OrationiDatabaseContext context, Guid requestId)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
 
-			if (requestId == null)
-				throw new ArgumentNullException(nameof(requestId));
+            if (requestId == null)
+                throw new ArgumentNullException(nameof(requestId));
 
-			_dbContext = context;
-			_requestId = requestId;
-		}
+            _dbContext = context;
+            _requestId = requestId;
+        }
 
-		public Response GenerateByMessageAndRequestId()
-		{
-			Message message = _dbContext.Messages.FirstOrDefault(m => m.Id == _requestId);
+        public Response GenerateByMessageAndRequestId()
+        {
+            Message message = _dbContext.Messages.FirstOrDefault(m => m.Id == _requestId);
 
-			if (message is null)
-				return GetNullResponse(_requestId);
+            if (message is null)
+                return GetNullResponse(_requestId);
 
-			if (message.StatusCode == (int)MessageStatusCodes.New)
-				return GetNewResponse(message);
+            if (message.StatusCode == (int)MessageStatusCodes.New)
+                return GetNewResponse(message);
 
-			if (message.StatusCode == (int)MessageStatusCodes.Preparing)
-				return GetPreparingResponse(message);
+            if (message.StatusCode == (int)MessageStatusCodes.Preparing)
+                return GetPreparingResponse(message);
 
-			if (message.StatusCode == (int)MessageStatusCodes.InProgress)
-				return GetInProgressResponse(message);
+            if (message.StatusCode == (int)MessageStatusCodes.InProgress)
+                return GetInProgressResponse(message);
 
-			if (message.StatusCode == (int)MessageStatusCodes.Processed)
-				return GetInProcessedResponse(message);
+            if (message.StatusCode == (int)MessageStatusCodes.Processed)
+                return GetInProcessedResponse(message);
 
-			if (message.StatusCode == (int)MessageStatusCodes.Error)
-				return GetErrorResponse(message);
+            if (message.StatusCode == (int)MessageStatusCodes.Error)
+                return GetErrorResponse(message);
 
-			// If we are here then someone added new status to DB and forgot to process it
-			throw new InvalidOperationException(
-				$"Couldn't generate response for status {message.StatusCode} in message {message.Id}");
-		}
+            // If we are here then someone added new status to DB and forgot to process it
+            throw new InvalidOperationException(
+                $"Couldn't generate response for status {message.StatusCode} in message {message.Id}");
+        }
 
-		private Response GetNullResponse(Guid requestId)
-		{
-			return new Response()
-			{
-				Id = requestId,
-				IsError = true,
-				Error = $"Request {requestId} not found",
-				Body = null
-			};
-		}
+        private Response GetNullResponse(Guid requestId)
+        {
+            return new Response()
+            {
+                Id = requestId,
+                IsError = true,
+                Error = $"Request {requestId} not found",
+                Body = null
+            };
+        }
 
-		private Response GetNewResponse(Message message)
-		{
-			return new Response()
-			{
-				Id = message.Id,
-				IsError = false,
-				Error = null,
-				Body = $"New. Wait for queing."
-			};
-		}
+        private Response GetNewResponse(Message message)
+        {
+            return new Response()
+            {
+                Id = message.Id,
+                IsError = false,
+                Error = null,
+                Body = $"New. Wait for queing."
+            };
+        }
 
-		private Response GetPreparingResponse(Message message)
-		{
-			return new Response()
-			{
-				Id = message.Id,
-				IsError = false,
-				Error = null,
-				Body = $"Preparing for processing."
-			};
-		}
+        private Response GetPreparingResponse(Message message)
+        {
+            return new Response()
+            {
+                Id = message.Id,
+                IsError = false,
+                Error = null,
+                Body = $"Preparing for processing."
+            };
+        }
 
-		private Response GetInProgressResponse(Message message)
-		{
-			return new Response()
-			{
-				Id = message.Id,
-				IsError = false,
-				Error = null,
-				Body = $"Processing."
-			};
-		}
+        private Response GetInProgressResponse(Message message)
+        {
+            return new Response()
+            {
+                Id = message.Id,
+                IsError = false,
+                Error = null,
+                Body = $"Processing."
+            };
+        }
 
-		private Response GetInProcessedResponse(Message message)
-		{
-			return new Response()
-			{
-				Id = message.Id,
-				IsError = false,
-				Error = null,
-				Body = message.ResponseBody
-			};
-		}
+        private Response GetInProcessedResponse(Message message)
+        {
+            return new Response()
+            {
+                Id = message.Id,
+                IsError = false,
+                Error = null,
+                Body = message.ResponseBody
+            };
+        }
 
-		private Response GetErrorResponse(Message message)
-		{
-			var resultResponse = new Response()
-			{
-				Id = message.Id,
-				IsError = true,
-				Body = message.ResponseBody
-			};
+        private Response GetErrorResponse(Message message)
+        {
+            var resultResponse = new Response()
+            {
+                Id = message.Id,
+                IsError = true,
+                Body = null
+            };
 
-			ProcessingError processingError = _dbContext.ProcessingErrors.FirstOrDefault(pe => pe.MessageId == message.Id);
-			if (processingError != null)
-			{
-				resultResponse.Error = processingError.Error;
-			}
+            ProcessingError processingError = _dbContext.ProcessingErrors.FirstOrDefault(pe => pe.MessageId == message.Id);
+            if (processingError != null)
+            {
+                resultResponse.Error = processingError.Error;
+            }
 
-			return resultResponse;
-		}
+            return resultResponse;
+        }
 
-		public static Response GenerateByException(Exception exception)
-		{
-			return new Response()
-			{
-				IsError = true,
-				Error = $"{exception.Message}, inner: {(exception.InnerException is null ? "no inner" : exception.InnerException.Message)}"
-			};
-		}
-	}
+        public static Response GenerateByException(Exception exception)
+        {
+            return new Response()
+            {
+                IsError = true,
+                Error = $"{exception.Message}, inner: {(exception.InnerException is null ? "no inner" : exception.InnerException.Message)}"
+            };
+        }
+    }
 }
