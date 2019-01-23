aeb2fad83730ffe149b3e0d47e4902a6dce1cf7b
Denis Vostrikov <eagl@bmail.ru>
2019-01-23 10:43:20 +0300

    #51 - Add new model and configuration to it

---
+++ KpdApps.Orationi.Messaging.DataAccess/EntityConfigurations/CallbackMessageTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using KpdApps.Orationi.Messaging.DataAccess.Models;
+
+namespace KpdApps.Orationi.Messaging.DataAccess.EntityConfigurations
+{
+    public class CallbackMessageTypeConfiguration : EntityTypeConfiguration<CallbackMessage>
+    {
+        public CallbackMessageTypeConfiguration()
+        {
+            ToTable("CallbackMessages");
+            
+            HasKey(p => p.Id)
+                .Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(p => p.Modified)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+        }
+    }
+}
