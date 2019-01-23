731851345d1a7a5c5fbb39de30b16e7476b17bdd
Denis Vostrikov <eagl@bmail.ru>
2019-01-23 10:44:21 +0300

    #51 - Add sql script for callbackmessage

---
+++ KpdApps.Orationi.Messaging.Database/dbo/Tables/CallbackMessages.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[CallbackMessages]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newid(), 
+    [MessageId] UNIQUEIDENTIFIER NOT NULL, 
+    [Modified] DATETIME NOT NULL DEFAULT getdate(), 
+    [CanBeSend] BIT NOT NULL DEFAULT 0, 
+    [WasSend] BIT NOT NULL DEFAULT 0, 
+    [StatusCode] INT NOT NULL DEFAULT 0, 
+    [ErrorMessage] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_CallbackMessages_Messages] FOREIGN KEY ([MessageId]) REFERENCES [dbo].[Messages]([Id])
+)
+
+GO
+
+CREATE unique INDEX [IX_CallbackMessages_MessageId] ON [dbo].[CallbackMessages] ([MessageId])
