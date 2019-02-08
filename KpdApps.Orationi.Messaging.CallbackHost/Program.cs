4a41190ffcb89330eb65a66389327b7fdddee90b
Denis Vostrikov <eagl@bmail.ru>
2019-02-08 16:50:40 +0300

    #51 - Add new project for callback service

---
+++ KpdApps.Orationi.Messaging.CallbackHost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceProcess;
+
+namespace KpdApps.Orationi.Messaging.CallbackHost
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var servicesToRun = new ServiceBase[]
+            {
+                new CallbackHost()
+            };
+            ServiceBase.Run(servicesToRun);
+        }
+    }
+}
